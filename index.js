dbae4232250670719bbc81828ca0da44546c1989
Pulkit Methaniya <79719013+Pulkit2099@users.noreply.github.com>
2023-04-21 01:45:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,8 +2,6 @@ const express =require("express")
 const app =express()
 const dotenv=require("dotenv")
 const mongoose=require("mongoose")
-
-const cors=require('cors')
 const authRoute=require("./routes/auth")
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
@@ -11,19 +9,27 @@ const categoryRoute = require("./routes/categories");
 const multer =require("multer")
 const path=require("path")
 const port =process.env.port||5000
+const cors=require('cors')
+
+const corsOrigin ={
+  origin:'http://localhost:3001', //or whatever port your frontend is using
+  credentials:true,            
+  optionSuccessStatus:200
+}
+app.use(cors(corsOrigin));
 
 
 dotenv.config()
 app.use(express.json())
 // app.use(cors())
 
-app.use(
-  cors({
-    origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: false,
-  })
-);
+// app.use(
+//   cors({
+//     origin: [process.env.FRONTEND_URL],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: false,
+//   })
+// );
 app.use("/images", express.static(path.join(__dirname, "/images")))
 
 
